7f2f1155f8b88089a407918bb948f48ef175d281
Greg <gmclean@cleansimple.ca>
2019-04-13 06:24:23 -0300

    starting work on bus ai

--- src/actors/bus.js
+++ src/actors/bus.js
@@ -1,16 +1,35 @@
-import {width_game, get_random_int} from '../helper'
+import { width_game, get_random_int } from '../helper'
+
+const max_speed = 0.006
+const min_speed = 0.002
 
 export default class Bus extends Phaser.Physics.Matter.Image {
   constructor(config) {
     super(config.scene.matter.world, get_random_int(0, width_game), get_random_int(0, -500), 'bus')
     this.scene = config.scene
-    this.setMass(40);
-    this.setAngle(270);
+    this.setMass(40)
+    this.setAngle(270)
     this.scene.add.existing(this);
+    this.speed = get_random_int(min_speed, max_speed)
   }
 
   update() {
-    //TODO: try to straighten out if crooked
-    this.thrustBack(0.005);
+    this.move_backwards();
+    this.angle_to_straight();
+
+  }
+
+  move_backwards() {
+    this.thrustBack(this.speed)
+  }
+
+  angle_to_straight() {
+    if (this.angle < -90) {
+      //this.setAngularVelocity(0.00001)
+      this.angle += 0.3
+    } else if (this.angle > -90) {
+      //this.setAngularVelocity(-0.00001)
+      this.angle -= 0.3
+    }
   }
 }
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,4 +1,4 @@
-import {width_game, height_game} from '../helper'
+import { width_game, height_game } from '../helper'
 import Player from '../actors/player';
 import Bus from '../actors/bus';
 import Tnt from '../actors/tnt'
@@ -14,12 +14,12 @@ export class Game extends Phaser.Scene {
 
   create() {
     this.make_background();
-    this.player = new Player({scene:this,x:50, y: 50})
+    this.player = new Player({ scene: this, x: 50, y: 50 })
 
     this.actor_array = [
-      new Bus({scene:this,x:200, y: 200}),
-      new Tnt({scene: this, x:100}),
-      new Bus({scene:this,x:300, y: 200})
+      new Bus({ scene: this, x: 200, y: 200 }),
+      new Tnt({ scene: this, x: 100 }),
+      new Bus({ scene: this, x: 300, y: 200 })
     ]
   }
 
@@ -27,14 +27,14 @@ export class Game extends Phaser.Scene {
     this.bg = this.add.tileSprite(
       0,
       0,
-      width_game*2,
-      height_game*2,
+      width_game * 2,
+      height_game * 2,
       'highway'
     );
   }
 
   update() {
-    this.bg.tilePositionY-=2
+    this.bg.tilePositionY -= 2
     this.player.update()
     this.actor_array.forEach((value) => {
       value.update()
@@ -43,12 +43,12 @@ export class Game extends Phaser.Scene {
   }
 
   respawner() {
-    this.actor_array.forEach((value, index, object)=>{
-      if(value.y > height_game) {
+    this.actor_array.forEach((value, index, object) => {
+      if (value.y > height_game) {
 
         value.destroy()
         object.splice(index, 1);
-        this.actor_array.push(new Bus({scene: this}))
+        this.actor_array.push(new Bus({ scene: this }))
       }
     })
   }
